6a6c973df45a9e0d057a252c46721aa4debce000
dagp-ucol <dgentner@ucol.mx>
2022-10-07 01:55:40 -0500

    Fix bug that does not allow to create food objects

---
+++ db/migrate/20221007065110_rename_type_column_in_food_table.rb
@@ -0,0 +1,5 @@
+class RenameTypeColumnInFoodTable < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :foods, :type, :food_type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_040240) do
+ActiveRecord::Schema.define(version: 2022_10_07_065110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_buffercache"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2022_10_07_040240) do
     t.string "name"
     t.integer "quantity"
     t.float "price"
-    t.string "type"
+    t.string "food_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
